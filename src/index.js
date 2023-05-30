d733e88b604d0bfb23c8392d7aee9568fc6ee979
dasha <dasha.danilchenko1990@gmail.com>
2023-05-30 19:51:09 +0300

    without hash router

--- src/index.js
+++ src/index.js
@@ -6,18 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store, { persistor } from './store';
 import { PersistGate } from 'redux-persist/lib/integration/react';
-import { HashRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate> 
     </Provider>
-    </HashRouter>
   </React.StrictMode>
 );
 

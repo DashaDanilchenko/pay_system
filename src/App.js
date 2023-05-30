8c8a4262a48a4bd401395063134dd236ddc19364
dasha <dasha.danilchenko1990@gmail.com>
2023-05-30 20:26:15 +0300

    import switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router";
 import Layout from './components/Layout';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
@@ -11,7 +11,7 @@ import Single from './components/Single';
 function App() {
   return (
    <Switch>
- <Route path='/pay_system/' element={<Layout/>}>
+      <Route path='/pay_system/' element={<Layout/>}>
         <Route index element={<Home/>}/>
         <Route path='/pay_system/cards' element={<Cards/>}/>
         <Route path='/pay_system/contrAgents' element={<ContrAgents/>}/>
